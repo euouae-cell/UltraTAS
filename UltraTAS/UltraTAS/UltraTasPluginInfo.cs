c820fe5bdf0f74ff8aae296dbeb32feaca1feb9c
euouae-cell <klomberson@gmail.com>
2026-08-31 20:00:54 +0300

    Delete UltraTAS/UltraTAS directory
    this is now the ultraBROKEN folder

--- UltraTAS/UltraTAS/UltraTasPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UltraTAS
-{
-    public static class UltraTasPluginInfo
-    {
-        public const string Name = "UltraTAS";
-        public const string GUID = "UltraTAS";
-        public const string Version = "1.0.0";
-    }
-}
